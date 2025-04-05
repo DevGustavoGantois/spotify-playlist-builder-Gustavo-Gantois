e7840c37ea8fefcc9890a395b2c6b5926f79e948
Gustavo Gantois <gugagantois@gmail.com>
2025-04-05 17:14:13 -0300

    test:Testing page index.ts.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import { useSession } from 'next-auth/react'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const {data: session} = useSession()
+  const [x, setX] = useState("")
+
+  useEffect(() => {
+    if (session && session.accessToken) {
+      setX(session.accessToken)
+    }
+  }, [session])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
