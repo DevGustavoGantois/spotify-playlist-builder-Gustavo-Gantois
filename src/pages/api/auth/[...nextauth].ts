5dc535c8e3c977afeb6ba1ee4bde26f4f4c6b3f4
Gustavo Gantois <gugagantois@gmail.com>
2025-04-05 16:03:55 -0300

    feat:Creating [...nextauth].ts for auth.

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify";
+
+const scopes = [
+    "user-read-email",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+    "user-read-currently-playing",
+    "user-modify-playback-state",
+].join(",")
+
+const params = {
+    scope: scopes,
+};
+
+const LOGIN_URL = "https://accounts.spotify.com/authorize?" + new URLSearchParams(params).toString();
+
+export const authOptions = {
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID as string,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+            authorization: LOGIN_URL,
+        })
+    ],
+    secret: process.env.JWT_SECRET,
+    pages: {
+        signIn: "/login"
+    },
+    callbacks: {
+        
+    }
+}
