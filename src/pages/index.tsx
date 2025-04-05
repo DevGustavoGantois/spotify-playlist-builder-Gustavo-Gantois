b91810ae87664e79f27fb3dbe6d482e91b760f49
Gustavo Gantois <gugagantois@gmail.com>
2025-04-05 16:13:19 -0300

    feat:Adding useSession in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
+import { useSession } from 'next-auth/react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const {data: session} = useSession()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+
     </main>
   )
 }
