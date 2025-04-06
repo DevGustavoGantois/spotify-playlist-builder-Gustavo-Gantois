06560b2cc92fec4590f7d47949527a05c8a93f53
Gustavo Gantois <gugagantois@gmail.com>
2025-04-06 09:59:08 -0300

    style:Adjusting playlist.name in sidebar component.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,10 +1,38 @@
+"use client";
+import { PlaylistProps } from "@/interface";
 import styles from "@/styles/sidebar.module.css";
-import { FaBook, FaHome, FaSearch } from "react-icons/fa";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { FaBook, FaHeart, FaHome, FaMusic, FaSearch } from "react-icons/fa";
 
 export function Sidebar() {
+
+    const { data: session } = useSession();
+    const [x, setX] = useState("");
+    const [userPlaylists, setUserPlaylists] = useState<PlaylistProps[]>([]);
+
+    useEffect(() => {
+        async function funcionality() {
+            if (session && session.accessToken) {
+              setX(session.accessToken)
+              const response = await fetch("https://api.spotify.com/v1/me/playlists", {
+                headers: {
+                  Authorization: `Bearer ${session.accessToken}`
+                }
+              })
+              const data = await response.json()
+              setUserPlaylists(data.items)
+            }
+          }
+      
+          funcionality()
+        }, [session])      
+
     return (
         <aside className={styles.sidebar}>
-            <div>SPOTIFY</div>
+            <Image src="/SpotifyLogo.png" width={50} height={50} priority alt="" />
+            <div className={styles.container}>
             <div className={styles.containerButton}>
                 <button className={styles.button}>
                     <FaHome className={styles.icon} />
@@ -18,6 +46,24 @@ export function Sidebar() {
                     <FaBook className={styles.icon} />
                     Sua biblioteca
                 </button>
+            </div>
+            <div className={styles.containerButton}>
+            <button className={styles.button}>
+                    <FaMusic className={styles.icon} />
+                    Crie sua Playlist
+            </button>
+            <button className={styles.button}>
+                    <FaHeart className={styles.icon} />
+                    Músicas curtidas
+            </button>
+            </div>
+            </div>
+            <div className={styles.containerPlaylists}>
+                {userPlaylists.map((playlist) => {
+                    return (
+                        <p className={styles.chooseMusic} key={playlist.id}>{playlist.name.length > 25 ? playlist.name.slice(0, 25) + "..." : playlist.name}</p>
+                    )
+                })}
             </div>
         </aside>
     )
