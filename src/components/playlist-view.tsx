edd7b0e5e70808bcc1c24c60597f2eb7c639f0ea
Gustavo Gantois <gugagantois@gmail.com>
2025-04-05 23:16:51 -0300

    feat:Create playlist-view component.

---
+++ src/components/playlist-view.tsx
@@ -0,0 +1,7 @@
+export function PlaylistView() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,7 @@
 export function Sidebar() {
     return (
-        <div>
+        <aside>
             
-        </div>
+        </aside>
     )
 }
