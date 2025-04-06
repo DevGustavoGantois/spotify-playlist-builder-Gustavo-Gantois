0ff6e9cf67b79ed4356ef225644afdc81f464051
Gustavo Gantois <gugagantois@gmail.com>
2025-04-05 23:44:51 -0300

    styles:Adjusting styles in sidebar component.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,15 @@
 import styles from "@/styles/sidebar.module.css";
+import { FaHome } from "react-icons/fa";
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <div>SPOTIFY</div>
             <div>
-                <button>Home</button>
+                <button className={styles.button}>
+                    <FaHome className={styles.icon} />
+                    Home
+                </button>
             </div>
         </aside>
     )
