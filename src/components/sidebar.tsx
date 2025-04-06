1d604f0a1e87ac1e0480083ca5dd8ed988244c09
Gustavo Gantois <gugagantois@gmail.com>
2025-04-05 23:49:58 -0300

    style:Adjusting button styles in sidebar.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,22 @@
 import styles from "@/styles/sidebar.module.css";
-import { FaHome } from "react-icons/fa";
+import { FaBook, FaHome, FaSearch } from "react-icons/fa";
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <div>SPOTIFY</div>
-            <div>
+            <div className={styles.containerButton}>
                 <button className={styles.button}>
                     <FaHome className={styles.icon} />
                     Home
+                </button>
+                <button className={styles.button}>
+                    <FaSearch className={styles.icon} />
+                    Pesquisar
+                </button>
+                <button className={styles.button}>
+                    <FaBook className={styles.icon} />
+                    Sua biblioteca
                 </button>
             </div>
         </aside>
