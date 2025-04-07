a8b5176f01bbb79a8aa90d398a7c3159f14b26ab
Gustavo Gantois <gugagantois@gmail.com>
2025-04-07 14:55:51 -0300

    style:Adjusting styles in player module css.

--- src/components/c-player.tsx
+++ src/components/c-player.tsx
@@ -1,9 +1,18 @@
 import styles from '@/styles/player.module.css';
+import { FaPlayCircle } from 'react-icons/fa';
 
 export function Player() {
     return (
         <section className={styles.playerSection}>
+            <div className={styles.playerDiv}>
 
+            </div>
+            <div className={styles.divIcon}>
+            <FaPlayCircle className={styles.icon} />
+            </div>
+            <div>
+                
+            </div>
         </section>
     )
 }
