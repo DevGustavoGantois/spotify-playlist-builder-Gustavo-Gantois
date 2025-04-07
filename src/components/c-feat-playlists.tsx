ac2f4d2d201d63640e8fab96ddf38e6ab017ed87
Gustavo Gantois <gugagantois@gmail.com>
2025-04-07 20:20:34 -0300

    feat:Create component search results and feat-playlists.

---
+++ src/components/c-feat-playlists.tsx
@@ -0,0 +1,7 @@
+export function FeatPlaylists() {
+    return (
+        <section>
+            
+        </section>
+    )
+}
---
+++ src/components/c-search-results.tsx
@@ -0,0 +1,7 @@
+export function searchResults() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
